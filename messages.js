d94b13304247fda6c77a75ca40eac9a9ae2fc78d
tioguda <guda.flavio@gmail.com>
2020-09-30 01:18:57 -0300

    Update message

--- messages.js
+++ messages.js
@@ -3,5 +3,5 @@ module.exports = {
   repositorio: 'https://manjariando.com.br/2018/07/14/repositorio-do-blog',
   apoiase: 'https://apoia.se/manjariando',
   chavegpg: 'https://manjariando.com.br/2017/03/18/aur-corrigir-erro-de-chave-publica-desconhecida',
-  comandos: '/pesquisa termo da pesquisa = pesquisa no blog o termo utilizado para a pesquisa.\n\n/repositorio = informa o link do tutorial para adicionar o repositório do blog.\n\n/apoiase = informa o link para o nosso apoia-se.\n\n/chavegpg = informa o tutorial de como adicionar a chave faltando.\n\n/comandos = mostra essa mensagem.',
+  comandos: '/pesquisa palavra-chave = pesquisa no blog a palavra chave utilizada para a pesquisa.\n\n/repositorio = informa o link do tutorial para adicionar o repositório do blog.\n\n/apoiase = informa o link para o nosso apoia-se.\n\n/chavegpg = informa o tutorial de como adicionar a chave faltando.\n\n/comandos = mostra essa mensagem.',
 }
