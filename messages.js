63663ed7cd6954a0f721c24ed3952c4e725fcb4f
tioguda <guda.flavio@gmail.com>
2021-05-16 04:46:19 -0300

    Update branchcompare command

--- messages.js
+++ messages.js
@@ -6,7 +6,7 @@ module.exports = {
   dualboot: 'https://manjariando.com.br/2018/11/14/dual-boot-com-o-manjaro',
   grub: 'Recuperar grub EFI\nhttps://manjariando.com.br/2018/06/05/recuperar-grub-efi\n\nRestaurar grub Legacy\nhttps://manjariando.com.br/2018/10/24/restaurar-grub-legacy',
   pastatmp: 'https://manjariando.com.br/2017/04/17/corrigir-erro-nao-ha-espaco-disponivel-no-dispositivo',
-  branchcompare: 'https://branch-compare.manjariando.com.br',
+  branchcompare: 'Manjaro\nhttps://manjaro.org/branch-compare\n\nManjariando\nhttps://branch-compare.manjariando.com.br',
   comandos: '/pesquisa palavra-chave = pesquisa no blog a palavra chave utilizada para a pesquisa.\n\n/repositorio = informa o tutorial para adicionar o repositório do blog.\n\n/apoiase = informa o nosso apoia-se.\n\n/chavegpg = informa o tutorial de como adicionar a chave faltando.\n\n/dualboot = informa o tutorial sobre dual boot no Manjaro.\n\n/grub = informa o tutorial para recuperar o grub.\n\n/pastatmp = informa o tutorial para ajudar com o problema.\n\n/branchcompare = informa a página para comparação das versões dos pacotes nos ramos stable e testing.\n\n/comandos = mostra essa mensagem.',
   by: '\n\nEspero ver você no grupo Manjariando em um futuro próximo!!!',
 }
